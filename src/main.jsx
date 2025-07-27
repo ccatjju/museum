c3312720f2f3f184154a70567967ca95c6b4fdd6
sulbum yun <sulbumyun@sulbumui-MacBookPro.local>
2025-07-27 16:59:49 +0900

    rout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Lights from "./component/Lights.jsx";
 import Test from "./component/Test.jsx";
 
